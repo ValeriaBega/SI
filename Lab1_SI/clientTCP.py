58c9c7c8317c14442b46aa9cd8e604b6eb1e07c9
ValeriaBega <begavaleria13@gmail.com>
2017-10-11 10:25:13 +0300

    send a file feature

--- Lab1_SI/clientTCP.py
+++ Lab1_SI/clientTCP.py
@@ -4,13 +4,32 @@ import socket
 
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_PORT = 5000
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = "Huehuehuehue!"
 
+#creating the tcp socket, sockstream indicating its type
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connection to hostname on the port
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+'''
+with open('received_file', 'wb') as f:
+    print 'file opened'
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
+        if not data:
+            break 
+        # write data to a file
+        f.write(data)
+
+f.close()
+'''
+
+#send a message
+s.send(MESSAGE.encode('utf-8'))
 data = s.recv(BUFFER_SIZE)
 s.close()
 
