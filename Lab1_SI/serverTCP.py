57ae36f37a18fd965f820941b4aca9b784d8090e
ValeriaBega <begavaleria13@gmail.com>
2017-10-11 10:22:47 +0300

    tcp with file sending

--- Lab1_SI/serverTCP.py
+++ Lab1_SI/serverTCP.py
@@ -4,13 +4,20 @@ import socket
 
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+TCP_PORT = 5000
+BUFFER_SIZE = 1024
 
+#creating the tcp socket, sockstream indicating its type
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#bind the server to the indicated port/addr
 s.bind((TCP_IP, TCP_PORT))
+
+#indicates the number of queued connections
 s.listen(5)
 
+#conn is a new socket to send/recieve data on this connection and addr is the addr bound to this connection
+#actually creates a new socket(??)
 conn, addr = s.accept()
 print 'Connection address:', addr
 while 1:
@@ -18,4 +25,15 @@ while 1:
     if not data: break
     print "received data:", data
     conn.send(data)  # echo
+'''
+    filename = 'mytext.txt'
+    f = open(filename, 'rb')
+    l = f.read(1024)
+    while (l):
+        conn.send(l)
+        print('Sent ', repr(l))
+        l = f.read(1024)
+    f.close()
+'''
+
 conn.close()
